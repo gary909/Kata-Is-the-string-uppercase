b11dd64b17edb43ebe03d5e1eb8bf645a73d8be2
gary <gary.white@lodgit.com>
2021-12-20 14:33:29 +0100

    code refactored

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function isUpperCase(myString) {
-    if (myString == '') {
-        return undefined;
-    } else if (myString == myString.toUpperCase()){
-        return true;
+// function isUpperCase(myString) {
+//     if (myString == '') {
+//         return undefined;
+//     } else if (myString == myString.toUpperCase()){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+//refactor into prototype:
+
+String.prototype.isUpperCase = function() {
+    if (this == this.toUpperCase()){
+        return true
     } else {
         return false;
     }
@@ -13,3 +23,4 @@ console.log(isUpperCase('c')); // return false
 console.log(isUpperCase('C')); // return true
 console.log(isUpperCase('hello I AM DONALD')); // return false
 console.log(isUpperCase('HELLO I AM DONALD')); // return true
+
