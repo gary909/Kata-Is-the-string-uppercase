83819c420f7530395dc232544582c13fbdaec4f1
gary <gary.white@lodgit.com>
2021-12-20 14:02:18 +0100

    code working

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-String.prototype.isUpperCase = function() {
-    // your code here
+function isUpperCase(myString) {
+    if (myString == '') {
+        return undefined;
+    } else if (myString == myString.toUpperCase()){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(isUpperCase('')); // return 'Undefined'
-console.log(isUpperCase('c is not upper case')); // return false
-console.log(isUpperCase('C is upper case')); // return true
+console.log(isUpperCase('c')); // return false
+console.log(isUpperCase('C')); // return true
 console.log(isUpperCase('hello I AM DONALD')); // return false
 console.log(isUpperCase('HELLO I AM DONALD')); // return true
